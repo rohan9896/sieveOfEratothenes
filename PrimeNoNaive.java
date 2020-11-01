88144a8e733eaa6cebfed9cae16831b25db7adff
Rohan Gupta <56235204+rohan9896@users.noreply.github.com>
2020-11-01 19:03:12 +0530

    Add files via upload

---
+++ PrimeNoNaive.java
@@ -0,0 +1,23 @@
+public class PrimeNoNaive {
+    
+    static boolean isPrime(int n){
+        if(n == 1){
+            return false;
+        }
+        for(int i = 2; i < n; i++) {
+            if(n%i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+	public static void main(String[] args) {
+	    int n = 25;
+		for(int i = 1; i < n; i++){
+		    if(isPrime(i)){
+		        System.out.print(i+" ");
+		    }
+		}
+	}
+}
